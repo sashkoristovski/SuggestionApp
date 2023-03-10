f9902096b5bb1fb825698a2dbedd5d1d0f8b0493
Sashko Ristovski <sashko.ristovski@yahoo.co.uk>
2023-03-10 22:10:53 +0000

    DataAccess_created

---
+++ SuggestionAppLibrary/DataAccess/MangoStatusData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MangoStatusData : IStatusData
+{
+    private readonly IMongoCollection<StatusModel> _statuses;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "StatusData";
+
+    public MangoStatusData(IDbConnection db, IMemoryCache cache)
+    {
+        _cache = cache;
+        _statuses = db.StatusCollecton;
+    }
+    public async Task<List<StatusModel>> GetAllStatuses()
+    {
+        var output = _cache.Get<List<StatusModel>>(cacheName);
+        if (output is null)
+        {
+            var results = await _statuses.FindAsync(_ => true);
+            output = results.ToList();
+
+            _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+
+        }
+        return output;
+    }
+
+    public Task CreateStatus(StatusModel status)
+    {
+        return _statuses.InsertOneAsync(status);
+    }
+}
